77a1355d9b8cec882cd96a99688de0d27c1b1304
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-11-09 07:42:29 +0600

    Created Navbar, Introduction section, and resume section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
       fontFamily: {
         sans: ['DM Sans', 'sans-serif'],
       },
+      colors: {
+        'primary': '#121414',
+      },
+      // gradient:{
+      //   linear: ['bg-gradient-to-r from-[#F5BD4D] to-[#F89222]'],
+      // },
     },
   },
   plugins: [],
